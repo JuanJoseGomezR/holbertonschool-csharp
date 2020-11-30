0c01077d4d4725343c07f8a14a907603e49006e7
JuanJoseGomezR <juangomez0149@gmail.com>
2020-11-30 20:09:32 +0000

    [Uploading] task 4

--- 0x00-csharp-hello_world/4-print_integer/4-print_integer.cs
+++ 0x00-csharp-hello_world/4-print_integer/4-print_integer.cs
@@ -5,8 +5,7 @@ class Program
 	static void Main(string[] args)
 	{
 		int number = 972;
-		Console.Write("{0}", number);
-		Console.WriteLine(" Mission Street");
+		Console.WriteLine(number + " Mission Street");
         }
 }
 
