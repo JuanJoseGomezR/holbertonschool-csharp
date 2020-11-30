2cd9402432dc143a55e649fd2a3e8a8f92b82185
JuanJoseGomezR <juangomez0149@gmail.com>
2020-11-30 20:01:48 +0000

    [Fixed] File Location

---
+++ 0x00-csharp-hello_world/4-print_integer/4-print_integer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		int number = 972;
+		Console.Write("{0}", number);
+		Console.WriteLine(" Mission Street");
+        }
+}
+
