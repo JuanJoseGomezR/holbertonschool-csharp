210f989d45ca90970ab13941d0f2d02ee767ca27
JuanJoseGomezR <juangomez0149@gmail.com>
2020-12-01 16:56:35 +0000

    [Uploading] Print Combinations of 2 Digit #

---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _2_print_alphabet
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string coma= "";
+			int i;
+			int j;
+			for (i = 0; i <= 9; i++)
+			{ 
+				for (j = i + 1; j <= 9; j++)
+				{
+					Console.Write("{0}{1}{2}",coma, i, j);
+					coma = ", ";
+				} 
+			}
+			Console.Write("\n");
+		}
+	}
+}
+
