39974e654a34970360105624d4a3ae00b331a147
JuanJoseGomezR <juangomez0149@gmail.com>
2020-12-01 20:24:42 +0000

    [Uploading] Task 11

---
+++ 0x01-csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Line
+{
+	public static void PrintDiagonal(int length)
+	{
+		int x;
+		int y;
+		if (length > 0)
+		{
+			for (x = 0; x < length; x++)
+			{
+				for (y = 0; y < x; y++)
+				{
+					Console.Write(" ");
+				}
+				Console.WriteLine("\\");
+			}
+		}
+		else
+		{
+			Console.WriteLine();
+		}
+	}
+}
+
