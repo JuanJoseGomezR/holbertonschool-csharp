afc086d0ce268c317affae24ec8fd88bc8ed09cd
JuanJoseGomezR <juangomez0149@gmail.com>
2020-12-02 18:49:08 +0000

    [Uploading] Create List

---
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class List
+{
+	public static List<int> CreatePrint(int size)
+	{
+		if (size < 0)
+		{
+			Console.WriteLine("Size cannot be negative");
+			return (null);
+		}
+		List<int> new_list = Enumerable.Range(0, size).ToList();
+		Console.WriteLine(string.Format("{0}", string.Join(" ", new_list)));
+		return (new_list);
+	}
+}
+
