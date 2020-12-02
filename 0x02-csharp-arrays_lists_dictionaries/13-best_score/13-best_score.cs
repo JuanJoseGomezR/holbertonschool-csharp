7ea97c23d04160c7d751ec287a52c03db38723ac
JuanJoseGomezR <juangomez0149@gmail.com>
2020-12-02 21:10:33 +0000

    [Uploading] Best Scorer name

---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Dictionary
+{
+	public static string BestScore(Dictionary<string, int> myList)
+	{
+		string name = "";
+		if (myList.Count < 1)
+		{
+			return "None";
+		}
+		foreach (KeyValuePair<string, int> reco in myList)
+		{
+			int bigg = myList.Values.Max();
+			if (reco.Value == bigg)
+			{
+				name = reco.Key;
+			}
+		}
+		return name;
+	}
+}
+
