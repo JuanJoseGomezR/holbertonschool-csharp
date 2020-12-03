291090af401b82bfd0caf6902297b7032a43a60b
JuanJoseGomezR <juangomez0149@gmail.com>
2020-12-03 22:57:24 +0000

    [Uploading] Task 9

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+	public static int Pop(LinkedList<int> myLList)
+	{
+		if (myLList.Count < 1)
+		{
+			return 0;
+		}
+		int deleted = myLList.First.Value;
+		myLList.RemoveFirst();
+		return deleted;
+	}
+}
+
