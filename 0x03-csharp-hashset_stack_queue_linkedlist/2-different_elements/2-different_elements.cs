f24c5c678448b37729eb9d391483df72ba10d118
JuanJoseGomezR <juangomez0149@gmail.com>
2020-12-03 15:56:18 +0000

    [Uploading] Task 2

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+	public static List<int> DifferentElements(List<int> list1, List<int> list2)
+	{
+		List<int> newlist = new List<int>();
+
+		foreach (int num in list1)
+		{
+			if (list2.Contains(num))
+			{
+				continue;
+			}
+			newlist.Add(num);
+		}
+		foreach (int num2 in list2)
+		{
+			if (list1.Contains(num2))
+			{
+				continue;
+			}
+			newlist.Add(num2);
+		}
+		newlist.Sort();
+		return newlist;
+	}
+}
+
