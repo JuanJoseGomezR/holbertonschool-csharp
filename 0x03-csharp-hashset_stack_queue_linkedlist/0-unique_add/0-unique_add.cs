30b40081761bac35deb1627e4f71393b051b0778
JuanJoseGomezR <juangomez0149@gmail.com>
2020-12-03 15:34:37 +0000

    [Uploading] Add Unique Ints in a list

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class List
+{
+	public static int Sum(List<int> myList)
+	{
+		int result = 0;
+		foreach (int nums in myList.Distinct().ToList())
+		{
+			result += nums;
+		}
+		return (result);
+	}
+}
+
