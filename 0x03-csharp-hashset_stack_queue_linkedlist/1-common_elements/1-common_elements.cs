0af4695c9809aa1a36d655968169c199a534980d
JuanJoseGomezR <juangomez0149@gmail.com>
2020-12-03 15:50:08 +0000

    [Uploading] Task 1

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+	public static List<int> CommonElements(List<int> list1, List<int> list2)
+	{
+		List<int> newlist = new List<int>();
+
+		foreach (int num in list1)
+		{
+			if (list2.Contains(num))
+			{
+				newlist.Add(num);
+			}
+		}
+		return newlist;
+	}
+}
+
