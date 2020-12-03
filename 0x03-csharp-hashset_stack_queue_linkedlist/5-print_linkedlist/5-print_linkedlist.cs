f22f7757bcdb9cac0207e8f1937a7c7d290c6a3b
JuanJoseGomezR <juangomez0149@gmail.com>
2020-12-03 21:22:49 +0000

    [Uploading] Task 5

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+	public static LinkedList<int> CreatePrint(int size)
+	{
+		LinkedList<int> newllist = new LinkedList<int>();
+
+		for (int i = 0; i < size; i++)
+		{
+			newllist.AddLast(i);
+		}
+		foreach (int item in newllist)
+		{
+			Console.WriteLine(item);
+		}
+		return newllist;
+	}
+}
+
