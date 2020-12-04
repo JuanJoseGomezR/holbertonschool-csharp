847ebb8242601c38a794a6fd6c67382cb0ebbc44
JuanJoseGomezR <juangomez0149@gmail.com>
2020-12-04 03:11:10 +0000

    [Uploading] Task 12

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+	public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
+	{
+		LinkedListNode<int> node = myLList.First;
+		while (node != null)
+		{
+			if (n < node.Value)
+			{
+				return myLList.AddBefore(node, n);
+			}
+			node = node.Next;
+		}
+		return myLList.AddLast(n);
+	}
+}
+
