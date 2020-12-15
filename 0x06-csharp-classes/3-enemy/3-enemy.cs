8e69b9a4bb3b3602d36247d78d7ea12c346c4916
JuanJoseGomezR <juangomez0149@gmail.com>
2020-12-15 16:08:44 +0000

    [Uploading] Documented Task

---
+++ 0x06-csharp-classes/3-enemy/3-enemy.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+///<summary>namespace Enemies</summary>
+namespace Enemies
+{
+    ///<summary> new class zombie</summary>
+    public class Zombie
+    {
+        ///<summary> zombie's health</summary>
+        public int health;
+        ///<summary> sets health to 0 </summary>
+        public Zombie()
+        {
+            this.health = 0;
+        }
+        ///<summary> sets new value to health </summary>
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            else
+            {
+                this.health = value;
+            }
+        }
+        ///<summary> returns zombie's gelath </summary>
+        public int GetHealth()
+        {
+            return (this.health);
+        }
+    }
+}
