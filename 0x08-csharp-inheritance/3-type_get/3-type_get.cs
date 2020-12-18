62e01ae677a2e92299e152c34efc8f086a96a84e
JuanJoseGomezR <juangomez0149@gmail.com>
2020-12-18 23:56:15 +0000

    [Uploading] 3rd Task

---
+++ 0x08-csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Obj
+{
+	public static void Print(object myObj)
+	{
+		Type t = myObj.GetType();
+
+
+		Console.WriteLine("{0} Properties:", t.Name);
+		foreach (var properties in t.GetProperties())
+		{
+			Console.WriteLine(properties.Name);
+		}
+		Console.WriteLine("{0} Methods:", t.Name);
+		foreach (var methods in t.GetMethods())
+		{
+			Console.WriteLine(methods.Name);
+		}
+	}
+}
