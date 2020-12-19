b1c2fe1c2105e86325f37e853ee55142878c1fcc
JuanJoseGomezR <juangomez0149@gmail.com>
2020-12-19 16:02:15 +0000

    [Uploading] task 7

---
+++ 0x08-csharp-inheritance/7-shape/7-shape.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+///<summary>Class Shape</summary>
+class Shape
+{
+	///<summary>Throw error "is not implemented"</summary>
+	public virtual int Area()
+	{
+		throw new System.NotImplementedException("Area() is not implemented");
+	}
+}
+
+///<summary> </summary>
+class Rectangle : Shape
+{
+	private int width;
+	private int height;
+	///<summary> Rectangle width </summary>
+	public int Width {
+		get {
+			return width;
+		}
+
+		set {
+			if (value < 0)
+			{
+				throw new System.ArgumentException("Width must be greater than or equal to 0");
+			}
+			else {
+				this.width = value;
+			}
+		}
+	}
+	///<summary> Rectangle height </summary>
+	public int Height
+	{
+		get
+		{
+			return height;
+		}
+		set
+		{	
+			if(value < 0)
+				throw new System.ArgumentException("Height must be greater than or equal to 0");
+			else
+				height = value;
+		}
+	}
+	///<summary> override method are </summary>
+	public override int Area()
+	{
+		return height * width;
+	}
+
+	public override string ToString()
+	{
+		return String.Format("[Rectangle] {0} / {1}", width, height);
+	}
+}
