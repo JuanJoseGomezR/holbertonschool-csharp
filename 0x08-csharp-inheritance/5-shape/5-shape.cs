a1684b0e026eda1be6ec55b3dc26139c9e553a7d
JuanJoseGomezR <juangomez0149@gmail.com>
2020-12-21 20:43:35 +0000

    [Uploading task 5

---
+++ 0x08-csharp-inheritance/5-shape/5-shape.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+///<summary>new class shapre</summary>
+class Shape
+{
+	///<summary>not implemented exception exception</summary>
+	public virtual int Area()
+	{
+		throw new System.NotImplementedException("Area() is not implemented");
+	}
+}
