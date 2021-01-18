5a9c6796c685cd598ab76986eb20a05dfc46d36a
JuanJoseGomezR <juangomez0149@gmail.com>
2021-01-18 15:24:16 +0000

    [Fixed] namespace removed

--- 0x09-csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
+++ 0x09-csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -1,24 +1,22 @@
 ﻿using System;
 
-namespace _9_vector_scalar_mul
+///<summary>multiply vector by scalar</summary>
+class VectorMath
 {
     ///<summary>multiply vector by scalar</summary>
-    class VectorMath
+    public static double[] Multiply(double[] vector, double scalar)
     {
-        ///<summary>multiply vector by scalar</summary>
-        public static double[] Multiply(double[] vector, double scalar)
+        double[] fail = {-1};
+        
+        if (vector.Length > 2)
         {
-            double[] fail = {-1};
-            
-            if (vector.Length > 2)
-            {
-                return fail;
-            }
-            double[] result = new double[vector.Length];
-            for (int i = 0; i < vector.Length; i++)
-            {
-                result[i] = vector[i] * scalar;
-            }
-            return result;
+            return fail;
         }
+        double[] result = new double[vector.Length];
+        for (int i = 0; i < vector.Length; i++)
+        {
+            result[i] = vector[i] * scalar;
+        }
+        return result;
     }
+}
