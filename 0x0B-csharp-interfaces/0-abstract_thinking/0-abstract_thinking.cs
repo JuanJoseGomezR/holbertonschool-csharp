cf75392dca4296b7a71ad8d51b5f606e6b95bddc
JuanJoseGomezR <juangomez0149@gmail.com>
2021-03-23 18:16:54 -0500

    Fixed Summaries

--- 0x0B-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
+++ 0x0B-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -3,8 +3,10 @@
 ///<summary>Abstrac class</summary>
 public abstract class Base
 {
+    /// <summary>Public property</summary>
     public string name;
 
+    /// <summary>Public property</summary>
     public override string ToString()
     {
         return name + " " + "is a " + GetType();
