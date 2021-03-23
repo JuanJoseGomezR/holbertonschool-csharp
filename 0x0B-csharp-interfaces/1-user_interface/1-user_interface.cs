4ccb87f880e1675393572b53a298759682a7dfc4
JuanJoseGomezR <juangomez0149@gmail.com>
2021-03-23 18:32:59 -0500

    [Uploading documentation]

--- 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
+++ 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -1,57 +1,53 @@
 ﻿using System;
 
-///<summary>Abstrac class</summary>
+///<summary>Base class</summary>
 public abstract class Base
 {
-    public string name;
-
+    ///<summary>Property name</summary>
+    private string name {get; set;}
+    ///<summary>String representation of the object</summary>
     public override string ToString()
     {
-        return name + " " + "is a " + GetType();
+        return $"{this.name} is a {this.GetType().Name}";
     }
 }
 
+
+///<summary>IInteractive interface</summary>
 public interface IInteractive
 {
+    ///<summary>Interact method</summary>
     void Interact();
 }
-
+///<summary>IBreakable interface</summary>
 public interface IBreakable
 {
-    int durability
-    { get; set; }
-
+    ///<summary>durability property</summary>
+    int durability {get; set;}
+    ///<summary>Break method</summary>
     void Break();
 }
-
+///<summary>ICollectable interface</summary>
 public interface ICollectable
 {
-    bool isCollected
-    { get; set; }
-
+    ///<summary>isCollected property</summary>
+    bool isCollected {get; set;}
+    ///<summary>Collect method</summary>
     void Collect();
 }
-
+///<summary>TestObject Class</summary>
 public class TestObject : Base, IInteractive, IBreakable, ICollectable
 {
-    public int durability
-    { get; set; }
-    /// <summary>Properties</summary>
-	public bool isCollected
-    { get; set; }
-    /// <summary>inherited method</summary>
-    public void Interact()
-    {
-
-    }
-    /// <summary>inherited method</summary>
-    public void Break()
-    {
-
-    }
-    /// <summary>inherited method</summary>
-    public void Collect()
-    {
-
-    }
+    ///<summary>durability property</summary>
+    public int durability {get; set;}
+    ///<summary>isCollected property</summary>
+    public bool isCollected {get; set;}
+    ///<summary>name property</summary>
+    public string name {get; set;}
+    ///<summary> Interact Method </summary>
+    public void Interact() {} 
+    ///<summary>Break Method</summary>
+    public void Break() {}   
+    ///<summary>Collect Method</summary>
+    public void Collect() {}
 }
