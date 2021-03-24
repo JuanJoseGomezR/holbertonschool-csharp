b87c9ea4229891d8d81df8dfdc3c131c44244cbe
JuanJoseGomezR <juangomez0149@gmail.com>
2021-03-24 23:50:26 +0000

    [Fixed] Variable typo

--- 0x0C-csharp-delegates_events/0-universal_health/0-universal_health.cs
+++ 0x0C-csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -4,25 +4,25 @@
 public class Player
 {
 	private string name;
-	private float maxHP;
+	private float maxHp;
 	private float hp;
 
 	///<summary>Constructor</summary>
-	public Player(string name="Player", float maxHP=100f)
+	public Player(string name="Player", float maxHp=100f)
 	{
-		if (maxHP <= 0)
+		if (maxHp <= 0)
 		{
-			this.maxHP = 100f;
+			this.maxHp = 100f;
 			Console.WriteLine("maxHP must be greater than 0. maxHp set to 100f by default");
 		}
-		this.hp = maxHP;
+		this.hp = maxHp;
 		this.name = name;
-		this.maxHP = maxHP;
+		this.maxHp = maxHp;
 	}
 
 	///<summary>Print health of player method</summary>
 	public void PrintHealth()
 	{
-		Console.WriteLine("{0} has {1} / {2} health", name, hp, maxHP);
+		Console.WriteLine("{0} has {1} / {2} health", name, hp, maxHp);
 	}
 }
