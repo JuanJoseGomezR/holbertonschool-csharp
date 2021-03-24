f462f32413d0a96ae93e991f65a54375e228e92b
JuanJoseGomezR <juangomez0149@gmail.com>
2021-03-23 19:03:03 -0500

    [Uploading] firrst Task

---
+++ 0x0B-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+/// <summary>Class Base</summary>
+public abstract class Base 
+{
+    /// <summary>Public property</summary>
+    public string name;
+    
+    /// <summary>Overides to string method</summary>
+        public override string ToString()
+        {
+            return name + " is a " + GetType();
+        }
+}
