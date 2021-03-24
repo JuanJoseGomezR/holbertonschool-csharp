b736d27a552e7e65d29f916d9a9d5b17e0c24ceb
JuanJoseGomezR <juangomez0149@gmail.com>
2021-03-24 17:00:15 +0000

    [Uploading] first Task

---
+++ 0x0C-csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+///<summary>Player class</summary>
+public class Player
+{
+	private string name;
+	private float maxHP;
+	private float hp;
+
+	///<summary>Constructor</summary>
+	public Player(string name="Player", float maxHP=100f)
+	{
+		if (maxHP <= 0)
+		{
+			this.maxHP = 100f;
+			Console.WriteLine("maxHP must be greater than 0. maxHp set to 100f by default");
+		}
+		this.hp = maxHP;
+		this.name = name;
+		this.maxHP = maxHP;
+	}
+
+	///<summary>Print health of player method</summary>
+	public void PrintHealth()
+	{
+		Console.WriteLine("{0} has {1} / {2} health", name, hp, maxHP);
+	}
+}
